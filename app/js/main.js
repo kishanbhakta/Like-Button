e4846ceb4482868cd3752be2a2b3f21c2ca90bfb
kishan bhakta <kishan.d.bhakta@gmail.com>
2015-06-23 21:04:45 -0400

    Got the clicks to work now adding styling

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,27 @@
-console.log('This is cool!');
+;(function (){
 
-$('.container').html(template.example({
-  value: 'Hello, World!!'
-}));
+  'use strict';
+
+  angular.module('likeBtnModule', [])
+
+  .controller('likeBtnController', ['$scope', function ($scope) {
+
+    $scope.allClicks = [];
+
+    var ClickCount = function (clickCount) {
+      this.count = 0;
+    };
+
+    $scope.amtClicked = function (amtClicked) {
+      var c = new ClickCount ();
+      c.count = +1;
+      $scope.allClicks.push(c);
+    };
+
+    $scope.counter = function (counter) {
+      return $scope.allClicks.length;
+    };
+
+  }]);
+
+}());
